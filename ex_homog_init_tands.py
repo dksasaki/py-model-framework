b2a1ca6248cdda2d236114659f02feb317dbd947
Default Seadas User <seadas-user@localhost>
2016-03-08 16:36:44 -0300

    exeample to make initial T and S

---
+++ ex_homog_init_tands.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import model_class.initial_conditions as ic
+
+ndepths = 15
+T=[27.59,27.59,26.88,25.57,23.02,21.76,20.26,18.73,15.99,12.43,
+                                      9.10,6.19,3.40,3.70,3.70];
+S=[36.87,36.87,36.90,36.93,36.95,36.85,36.69,36.42,35.79,35.27,34.81,
+                                       34.51,34.51,34.94,34.94];
+
+
+i_cond = ic.secom_initial_conditions()
+i_cond.define_grid_cells_hor_homog(ndepths,T,S)
+i_cond.define_grid_TS_values()
+i_cond.write_init_tands()
