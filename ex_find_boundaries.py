eae23d7ad9f2effb9a64424ddf72d6303bad3497
Default Seadas User <seadas-user@localhost>
2016-03-17 17:51:43 -0300

    code improvement

--- ex_find_boundaries.py
+++ ex_find_boundaries.py
@@ -6,14 +6,17 @@ import model_class.model_nesting as mn
 
 import model_class.places as pl
 
+#This script uses 2 grids. The mother grid and the nested grid.
+
+
 plt.ion()
 
 q = lambda f,ind : [f[i[0],i[1]] for i in ind]
 
 a = mn.secom_nesting()
-a.boundaries_location()
-a.nearest_boundaries_location()
-a.all_neigbours_nearest_bl()
+a.boundaries_location() 
+a.nearest_boundaries_location() #this method find the nearest points between the mother grid and the boundaries of the nested grid  
+a.all_neigbours_nearest_bl() #this method finds all the neighbors of the poinst defined by the self.neares_boundaires_location method
 
 b = pl.mapa()
 b.Embaiamento_sp()
@@ -24,6 +27,7 @@ b.mapa.plot(a.g_mask(a.c('lon'),0).T,a.g_mask(a.c('lat').T,0),'k',latlon=True)
 b.mapa.plot(a.xb,a.yb,'m', latlon=True)
 #b.mapa.plot(a.d[:,0],a.d[:,1],'.r',linewidth=3,latlon=True)
 
-for i,j in zip([0,0],[1,0],[-1,0],[0,1],[0,-1]):
-	b.mapa.plot(a.ann(i,j)[:,0],a.ann(i,j)[:,1],'or',latlon=True)
+#self.ann is a lambda function which finds the neighbours of self.nearest_boundaries_location, using its neightboindexes
+for i in a.ann: 
+	b.mapa.plot(i[0],i[1],'og',latlon=True)
 
