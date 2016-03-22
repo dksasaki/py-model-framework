1f2b7e907a90c0da698134937550a377746374e6
Default Seadas User <seadas-user@localhost>
2016-03-22 16:29:46 -0300

    program to interface implemented

--- model_class/model_nesting.py
+++ model_class/model_nesting.py
@@ -1,81 +1,82 @@
-from model_class.grid_class import secom_read
-import xray as xr
-import matplotlib.pyplot as plt
 import numpy as np
-import model_class.model_boundaries as mb
+from model_class.grid_class import secom_read
 from model_class.read_class import secom_read_data
+import model_class.model_boundaries as mb
 from scipy.spatial import kdtree as kd
+class model_nesting(object):
+    """
+    lon : mxn numpy matrix
+    lat : mxn numpy matrix
+    im  : mx1 numpy array - m direction indexes
+    indx: indexes of desired lon,lat pairs (the pairs are a (p*k)x1 list)
+            ex: [lon,lat] = [[20,30], [21,32], [22,33]]
+                if indx = 0
+                [lon,lat][indx] --> [20,30]
 
-class secom_nesting(secom_read_data):
     """
-    This method interpolates the data of a coarser grid in a finer grid.
-    - self.boundaries_location
-        Locates the boundaries of the model_grid file.
-    - self.nearest_boundaries_location.
-        Locates the closest poinst in the coarser grid related to the finer grid boundaries points.
-        They are defined as self.d
-    - self.all_neigbours_nearest_bl
-        
-    """
-    
     def __init__(self):
-        super(secom_nesting, self).__init__()
         self.zip_ar     = lambda x,y : np.array(zip(x,y))
         self.mtx_val    = lambda f,ind : [f[i[0],i[1]] for i in ind] #gets the a value of f (f is a mxn matrix),
                                                                      #according to the given ind indexes.
         self.zip_mtx_val= lambda f1,f2,p,q : np.array([self.mtx_val(f1,zip(p,q)), \
                                              self.mtx_val(f2,zip(p,q))]).T #pair of list, that get a the value of
-                                                                           #f1 and f2 in p and q positions
+        pass
 
 
-    def boundaries_location(self):
-        """
-        Gets the model_grid boundaries.
-        They are defined as:
-        - self.xb (model_grid longitude boundary)
-        - self.yb (model_grid latitude boundary)
-        """
-        bnd = mb.secom_model_boundaries()
-        bnd.find_boundaries()
-        self.xb = bnd.xb
-        self.yb = bnd.yb
-
-    def nearest_boundaries_location(self):
+    def nearest_boundaries_location(self,lon,lat):
         """
         This method find the nearest points between the mother grid and the boundaries of the nested grid.
         """
-        mdgrd =  self.zip_ar( self.c('lon').ravel(),self.c('lat').ravel() )  
-        #a nearest point search is determined by KDTtree, based on the model grid
+        mdgrd =  np.array(zip( lon.ravel(),lat.ravel() ))
         kdt = kd.KDTree(mdgrd)
+        self.mdi_dist, self.mdi = kdt.query(np.array(zip( self.xb,self.yb)) )
 
-        #nearest model grid distance and points to the desired boundaries
-        self.mdi_dist, self.mdi = kdt.query(self.zip_ar(self.xb,self.yb)) 
-        self.d = np.array([mdgrd[j].tolist() for j in self.mdi])
+    def all_neigbours_nearest_i_bl(self,indx,im):
+         """
+         This method finds all the neighbors of the poinst defined by the self.nearest_boundaries_location method.
+         It defines the self.ann - lon,lat of the neighbor points to the nearest points
+         """
+         self.i_pos   = np.array(indx)/im.max().astype('int') #lon indexes from the coarser grid
+         self.j_pos   = np.array(indx)%im.max().astype('int') #lat indexes from the coarser grid
 
-    def all_neigbours_nearest_bl(self):
-        """
-        This method finds all the neighbors of the poinst defined by the self.nearest_boundaries_location method.
-        It defines the self.ann the lon,lat of the neighbor points
-        """
-        self.i_pos   = np.array(self.mdi)/self.c('xpos').max().astype('int') #lon indexes from the coarser grid
-        self.j_pos   = np.array(self.mdi)%self.c('xpos').max().astype('int') #lat indexes from the coarser grid
+         ann_i= []
 
-        self.ann = []
+         for i in zip([0,1,-1,0,0],[0,0,0,1,-1]): 
+            ann_i.append([(self.i_pos+i[0]).tolist(),(self.j_pos+i[1]).tolist()])
 
-        for i in zip([0,1,-1,0,0],[0,0,0,1,-1]): 
-            aux = self.zip_mtx_val(self.c('lon'),self.c('lat'),self.i_pos+i[0],self.j_pos+i[1])
-            self.ann.append([aux[:,0],aux[:,1]])
+         self.ann_i = self.remove_repeated(ann_i)
 
-    def f7(seq):
-        seen = set()
-        seen_add = seen.add
-        return [x for x in seq if not (x in seen or seen_add(x))]
+    def all_neigbours_nearest_bl(self,lon,lat):
+        i = self.ann_i[:,0]
+        j = self.ann_i[:,1]
 
-        new_list = reduce(lambda x,y: x+[y][:1-int(y in x)], a.ann[0][0], []) ??
+        self.ann = [lon[i,j],lat[i,j]]
 
+    def remove_repeated(self,g):
+         m = np.array(g).shape[2]*np.array(g).shape[0]
+         n = np.array(g).shape[1]
+         d = np.array(g).transpose(2,0,1).reshape(m,n)
+         e = set(zip(d[:,0].tolist(),d[:,1].tolist()))
+         f = np.array([[i[0],i[1]] for i in  e])
 
-    #this method interpolates the data of the mother dataset to the nested boundaries
+         return f
 
 
-#mdgrd = np.array(zip(a.c('lon').ravel(),a.c('lat').ravel()))
+class secom(model_nesting,secom_read_data):
+    def __init__(self):
+         super(model_nesting, self).__init__()
+         super(secom_read_data, self).__init__()
 
+    def find_boundaries(self):
+         bnd = mb.secom_model_boundaries()
+         bnd.find_boundaries()
+         self.xb = bnd.xb
+         self.yb = bnd.yb
+
+
+    def nearest_boundaries(self):
+         self.nearest_boundaries_location(self.c('lon'),self.c('lat'))
+
+    def boundaries_nearest_neighbors(self):
+         self.all_neigbours_nearest_i_bl(self.mdi,self.c('xpos'))
+         self.all_neigbours_nearest_bl(self.c('lon'),self.c('lat'))
