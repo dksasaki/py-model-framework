8955b61def2b4bcffe84b7b0a500ece7c6248606
Default Seadas User <seadas-user@localhost>
2016-04-11 16:50:25 -0300

    minor changes

--- model_class/secom_interface.py
+++ model_class/secom_interface.py
@@ -17,12 +17,28 @@ class secom(model_nesting,secom_read,secom_read_data,boundaries):
          self.boundaries_coordinates(self.g(1),self.g(0),self.g(7),self.g(6),self.g(4))
          self.boundaries_grid(self.g(1),self.g(0),self.g(7),self.g(6),self.g(4))
 
-    def eta_boundaries(self,output_file):
+    def eta_boundaries_homog(self,output_file):
         print output_file
         self.define_eta_boundaries_values()
         self.define_eta_boundaries_i()
         self.define_eta_boundaries_homog()
         self.write_eta_boundaries(output_file)
+
+    def eta_boundaries_heter(self,output_file):
+        self.boundaries_nearest_neighbors()
+        self.define_eta_boundaries_values()
+        self.define_eta_boundaries_i()
+        self.define_eta_boundaries_homog()
+
+        x  = self.c('lon')[self.ann_i[:,0],self.ann_i[:,1]]
+        y  = self.c('lat')[self.ann_i[:,0],self.ann_i[:,1]]
+        eta= self.f_xr['elev'][10,:,:]
+        var = self.interpolate_coarser2finer_2D(x,y,self.xb,self.yb,eta.data,self.ann_i)
+        self.define_eta_boundaries_heter(var)
+        self.define_eta_boundaries_i()
+
+        self.write_eta_boundaries(output_file)
+
 
     def TS_boundaries_homog(self,ndepths,T,S,output_file):
         self.define_TS_boundaries_i()
@@ -43,7 +59,7 @@ class secom(model_nesting,secom_read,secom_read_data,boundaries):
         self.define_TS_boundaries()
 
         var = self.interpolate_coarser2finer(x,y,self.xb,self.yb,T.data,self.ann_i)
-        var = np.append(var,self.interpolate_coarser2finer(x,y,self.xb,self.yb,S.data,self.ann_i),axis=1)
+        var = np.append(var,self.interpolate_coarser2finer_3D(x,y,self.xb,self.yb,S.data,self.ann_i),axis=1)
         self.define_TS_values_heter(var[:,:5].T,var[:,5:].T,5)
         self.write_TS_boundaries(output_file)
 
