089d2d340cd4e9f57ae99dba5203bd6a76d1e417
Default Seadas User <seadas-user@localhost>
2016-04-11 14:54:59 -0300

    heterogenous TS boundaries method implemented

--- ex_find_boundaries.py
+++ ex_find_boundaries.py
@@ -5,11 +5,12 @@ import model_class.secom_interface as si
 plt.close('all')
 plt.ion()
 
-ndepths = 15
+ndepths = 5
 T=[27.59,27.59,26.88,25.57,23.02,21.76,20.26,18.73,15.99,12.43,
-                                      9.10,6.19,3.40,3.70,3.70];
+                                      9.10,6.19,3.40,3.70,3.70]; T = T[:ndepths]
 S=[36.87,36.87,36.90,36.93,36.95,36.85,36.69,36.42,35.79,35.27,34.81,
-                                       34.51,34.51,34.94,34.94];
+                                       34.51,34.51,34.94,34.94]; S = S[:ndepths]
+
 eta = 'eta_bound'
 
 
@@ -29,23 +30,40 @@ b.mapa.plot(a.ann[0],a.ann[1],'or',latlon=True) #plota os pontos da grade grossa
 b.mapa.plot(a.xb,a.yb,'m', latlon=True,linewidth=5) #plota os contornos da grade fina
 
 a.eta_boundaries(eta) #cria o arquivo eta_bound
-a.TS_boundaries(15,T,S,'homog_bound') #cria o arquivo homog_bound
+a.TS_boundaries_homog(5,T,S,'homog_bound') #cria o arquivo homog_bound
 
  #####
  #interpola
 from model_class.read_class import secom_read_data
 from scipy import interpolate
 
-a
+
 c = secom_read_data()
 
 #coarser grid
 x = c.c('lon')[a.ann_i[:,0],a.ann_i[:,1]]
 y = c.c('lat')[a.ann_i[:,0],a.ann_i[:,1]]
-T = c.f_xr['temp'][0,:,:]
-S = c.f_xr['salt'][0,:,:]
+T = c.f_xr['temp'][0,:,:,:]
+S = c.f_xr['salt'][0,:,:,:]
 
 #from model_class.model_boundaries import boundaries 
 #bnd = boundaries()
-var = a.interpolate_coarser2finerTS(x,y,a.xb,a.yb,T.data,S.data,a.ann_i)
-a.define_TS_values_heter(var[:,:,0].T,var[:,:,1].T,5)
+#var = a.interpolate_coarser2finer(x,y,a.xb,a.yb,T.data,a.ann_i)
+#var = np.append(var,a.interpolate_coarser2finer(x,y,a.xb,a.yb,S.data,a.ann_i),axis=1)
+#a.define_TS_values_heter(var[:,:5].T,var[:,5:].T,5)
+#a.write_TS_boundaries('TS_bounds')
+
+#bla = np.array([T[:,i[0],i[1]].data for i in a.ann_i]) #T in a.ann_i sites
+
+
+#ginterp = interpolate.griddata((x,y),bla[:,0],(a.xb,a.yb),method='linear')
+
+#b.mapa.scatter(a.xb,a.yb,c=ginterp,vmin=20,vmax=26,latlon=True,zorder=2)
+#b.mapa.colorbar()
+#b.mapa.scatter(x,y,c = np.array(bla[:,0]),vmin=20,vmax=26,latlon=True,zorder=3)
+#b.mapa.colorbar()
+#plt.savefig('teste_malha2.eps',bbox_inches='tight')
+
+
+####
+#eta boundaries
