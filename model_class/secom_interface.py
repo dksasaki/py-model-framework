3e3b386d0aa8deca3fa1a5288da09e826666d888
Default Seadas User <seadas-user@localhost>
2016-04-11 16:36:34 -0300

    eta boundaries implemented

--- model_class/secom_interface.py
+++ model_class/secom_interface.py
@@ -2,12 +2,13 @@ from model_class.grid_class import secom_read
 from model_class.read_class import secom_read_data
 from model_class.model_boundaries import boundaries
 from model_class.model_nesting import model_nesting
+import numpy as np
 
 class secom(model_nesting,secom_read,secom_read_data,boundaries):
     def __init__(self):
-         model_nesting.__init__(self)
-         secom_read.__init__(self)
-         secom_read_data.__init__(self)
+         model_nesting.__init__(self) 
+         secom_read.__init__(self)      #model_grid is read here
+         secom_read_data.__init__(self) #gcmplt.cdf is read here
          boundaries.__init__(self)
          self.find_boundaries()
         
@@ -20,19 +21,35 @@ class secom(model_nesting,secom_read,secom_read_data,boundaries):
         print output_file
         self.define_eta_boundaries_values()
         self.define_eta_boundaries_i()
-        self.define_eta_boundaries()
+        self.define_eta_boundaries_homog()
         self.write_eta_boundaries(output_file)
 
-    def TS_boundaries(self,ndepths,T,S,output_file):
+    def TS_boundaries_homog(self,ndepths,T,S,output_file):
         self.define_TS_boundaries_i()
         self.define_TS_values(ndepths)
-        self.define_TS_values_homog(T,S)
+        self.define_TS_values_homog(T,S,ndepths)
         self.define_TS_boundaries()
         self.write_TS_boundaries(output_file)
 
-    def nearest_boundaries(self):
-         self.nearest_boundaries_location(self.c('lon'),self.c('lat'))
+    def TS_boundaries_heter(self,ndepths,output_file):
+
+        x = self.c('lon')[self.ann_i[:,0],self.ann_i[:,1]]
+        y = self.c('lat')[self.ann_i[:,0],self.ann_i[:,1]]
+        T = self.f_xr['temp'][0,:,:,:]
+        S = self.f_xr['salt'][0,:,:,:]
+
+        self.define_TS_boundaries_i()
+        self.define_TS_values(ndepths)
+        self.define_TS_boundaries()
+
+        var = self.interpolate_coarser2finer(x,y,self.xb,self.yb,T.data,self.ann_i)
+        var = np.append(var,self.interpolate_coarser2finer(x,y,self.xb,self.yb,S.data,self.ann_i),axis=1)
+        self.define_TS_values_heter(var[:,:5].T,var[:,5:].T,5)
+        self.write_TS_boundaries(output_file)
+
+
 
     def boundaries_nearest_neighbors(self):
+         self.nearest_boundaries_location(self.c('lon'),self.c('lat'))
          self.all_neigbours_nearest_i_bl(self.mdi,self.c('xpos'))
          self.all_neigbours_nearest_bl(self.c('lon'),self.c('lat'))
